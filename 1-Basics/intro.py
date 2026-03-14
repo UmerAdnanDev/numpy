c07d134e6a20b771b9ca4cab1e9ce7f5dbf41af4
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-15 02:15:06 +0500

    added Basic folder and intro.py
    it has the basic intro of numpy

---
+++ 1-Basics/intro.py
@@ -0,0 +1,7 @@
+#Numpy a framework which is written in C++ to make python fast
+python_list =[1,2,3,4,5] #python list
+import numpy as np
+numpy_array  = np.array([1,2,3,4,5]) # numpy array
+print(python_list) #[1, 2, 3, 4, 5]
+print(numpy_array) #[1 2 3 4 5]
+print(type(numpy_array)) #<class 'numpy.ndarray'>

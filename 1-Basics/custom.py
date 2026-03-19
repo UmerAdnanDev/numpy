083f1c254207d71bf188e8ec8bbd04ea18e70608
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-19 23:57:07 +0500

    added custom array with full method

---
+++ 1-Basics/custom.py
@@ -0,0 +1,11 @@
+# full((shape),number)
+import numpy as np
+cus1_array = np.full((3,5),8)
+print(cus1_array)
+# [[8 8 8 8 8] 
+#  [8 8 8 8 8] 
+#  [8 8 8 8 8]]
+cus2_array = np.full((2,3),'$')
+print(cus2_array)
+# [['$' '$' '$'] 
+#  ['$' '$' '$']]

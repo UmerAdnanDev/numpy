f9520d17e0f92c3167701a05f04fcb32c9ca03c7
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-20 09:09:54 +0500

    added ndim method
    tells number of dimensions in an array

---
+++ 2-Attributes/ndim.py
@@ -0,0 +1,8 @@
+#ndim: number of dimensions
+import numpy as np
+array1d = np.array([1,2])
+array2d = np.array([[9,0,3]])
+array3d = np.array([[[9,0,3,0]]])
+print(array1d.ndim)
+print(array2d.ndim)
+print(array3d.ndim)

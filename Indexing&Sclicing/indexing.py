07efeb4aa25b039c96118cf49f808eb0b248e593
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-22 16:10:29 +0500

    adding indexing
    selecting a single element from array

---
+++ Indexing&Sclicing/indexing.py
@@ -0,0 +1,9 @@
+# in python items are stored in arrays and lists from 0 to end.. from l.h.s to r.h.s
+#and from -1 to start from r.h.s to l.h.s
+import numpy as np
+ar1 = [1,2,3,4,5,6]
+     # 0 1 2 3 4 5
+print(ar1[0])# 1
+print(ar1[-1])# 6
+print(ar1[3])# 4
+# print(ar1[20]) will cause Index error as no number exists at that point currently
